92cfaee52da3363a33c8f4b16b6c4f820334188e
Christopher Gervais <chris@ergonlogic.com>
2017-04-28 01:02:51 +0000

    Allow composer install to find proper directory.

--- drush/Provision/ComposerInstall.php
+++ drush/Provision/ComposerInstall.php
@@ -6,21 +6,42 @@
 
 class Provision_ComposerInstall extends Provision_ShellCommand {
 
-  // The prefix used for properties in Aegir contexts.
-  protected $context_prefix = 'git_';
+  // The local path in which we'll install dependencies.
+  protected $path = FALSE;
 
-  // List of properties to load from the Aegir context.
-  protected $context_properties = ['repository_path'];
+  public function __construct() {
+    $this->setPath();
+  }
 
-  // The local path to which we'll clone the repository.
-  protected $repository_path = FALSE;
+  protected function setPath() {
+    $this->path = $this->findPath();
+  }
+
+  protected function findPath() {
+    $paths = drush_command_invoke_all('composer_install_path');
+    drush_command_invoke_all_ref('composer_install_paths_alter', $paths);
+    $paths = array_filter($paths); // Filter NULL values.
+    $paths = array_filter($paths, 'is_dir'); // Ensure remaining paths exist.
+    switch (count($paths)) {
+      case 0:
+        $this->warning(dt('Could not find path for Composer install.'));
+        return FALSE;
+      case 1:
+        break;
+      default:
+        $this->warning(dt('Multiple candidate paths found:'));
+        $this->warning(implode("\n", $paths));
+        $this->warning(dt('Proceeding with: ' . $paths[0]));
+    }
+    return $paths[0];
+  }
 
   public function preProvisionVerify() {
     if (!$this->checkComposerInstall()) {
       return;
     }
-    if (!$this->pathExists($this->repository_path)) {
-      return $this->error(dt('Platform repository path does not exist. Cannot install dependencies. Aborting.'));
+    if (!$this->pathExists($this->path)) {
+      return $this->error(dt('Platform repository path (:path) does not exist. Cannot install dependencies. Aborting.', [':path' => $this->path]));
     }
     else {
       $this->notice(dt('Platform Git repository path exists.'));
@@ -46,7 +67,7 @@ class Provision_ComposerInstall extends Provision_ShellCommand {
 
   protected function checkComposerLockExists() {
     $this->notice(dt('Checking for `composer.lock` file.'));
-    if (file_exists($this->repository_path . '/composer.lock')) {
+    if (file_exists($this->path . '/composer.lock')) {
       $this->success(dt('Found `composer.lock` file. Proceeding with installation of dependencies.'));
       return TRUE;
     }
@@ -58,7 +79,7 @@ class Provision_ComposerInstall extends Provision_ShellCommand {
 
   protected function checkComposerJsonExists() {
     $this->notice(dt('Checking for `composer.json` file.'));
-    if (file_exists($this->repository_path . '/composer.json')) {
+    if (file_exists($this->path . '/composer.json')) {
       $this->success(dt('Found `composer.json` file. Proceeding with installation of dependencies.'));
       $this->warning(dt('Installing dependencies from `composer.json` does not guarantee a reproducible build. A `composer.lock` file should be committed to this Git repository.'));
       return TRUE;
@@ -75,7 +96,7 @@ class Provision_ComposerInstall extends Provision_ShellCommand {
   }
 
   protected function buildComposerInstallCommand() {
-    $command = 'cd ' . escapeshellarg(trim($this->repository_path));
+    $command = 'cd ' . escapeshellarg(trim($this->path));
     $command .= ' && composer install --no-progress --prefer-dist --no-interaction --no-suggest';
     return $command;
   }
